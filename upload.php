680e703512c83fb90a796620abb12078964c03b2
adli <redhunter42121@gmail.com>
2020-01-31 19:30:34 +0700

    add analyze feature

--- upload.php
+++ upload.php
@@ -27,7 +27,11 @@ if(isset($_POST['upload']))
     $createContainerOptions->addMetaData("key1", "value1");
     $createContainerOptions->addMetaData("key2", "value2");
  
-    $containerName = "blockblobs";
+    $containerName = "blockblobs".generateRandomString();
+
+    // Create container.
+    $blobClient->createContainer($containerName, $createContainerOptions);
+    
  
     // Sampai kode di atas kita telah membuat instancce Azure storage client, menginstansiasi objek blob service, membuat container baru, dan mengatur perijinan ke container agar blob bisa diakses oleh semua.
     
@@ -57,6 +61,7 @@ if(isset($_POST['upload']))
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Upload Image</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 </head>
 <body>
 <h1>Upload Image</h1>
@@ -64,6 +69,8 @@ if(isset($_POST['upload']))
       <input type="file" name="img" id=""> <br><br>
       <button type="submit" name="upload">Upload</button>
     </form>
+    <br>
+    <button onclick="processImage();">analyze image</button>
 
     <br>
     <br>
@@ -71,9 +78,81 @@ if(isset($_POST['upload']))
     <br>
     <br>
 
+<div style="display:flex;">
 <?php if(isset($_POST['upload'])) 
 {   ?>
-    <img src="<?php echo $imgUrl; ?>" alt="">
+    <img src="<?php echo $imgUrl; ?>" alt="" id="img" style="width:50%;">
 <?php } ?>
+<div id="jsonOutput" style="width:50%; display:table-cell;">
+        Response:
+        <br><br>
+        <textarea id="responseTextArea" class="UIInput"
+                  style="width:580px; height:400px;"></textarea>
+</div>
+
+</div>
+
+<script type="text/javascript">
+    function processImage() {
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
+ 
+        // Replace <Subscription Key> with your valid subscription key.
+        var subscriptionKey = "0fe1760638b54fbba01ea429f08e061a";
+ 
+        // You must use the same Azure region in your REST API method as you used to
+        // get your subscription keys. For example, if you got your subscription keys
+        // from the West US region, replace "westcentralus" in the URL
+        // below with "westus".
+        //
+        // Free trial subscription keys are generated in the "westus" region.
+        // If you use a free trial subscription key, you shouldn't need to change
+        // this region.
+        var uriBase =
+            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+ 
+        // Request parameters.
+        var params = {
+            "visualFeatures": "Categories,Description,Color",
+            "details": "",
+            "language": "en",
+        };
+ 
+        // Display the image.
+        var sourceImageUrl = "<?php echo $imgUrl; ?>";
+ 
+        // Make the REST API call.
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+ 
+            // Request headers.
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+ 
+            type: "POST",
+ 
+            // Request body.
+            data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        })
+ 
+        .done(function(data) {
+            // Show formatted JSON on webpage.
+            $("#responseTextArea").val(JSON.stringify(data, null, 2));
+        })
+ 
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ? "Error. " :
+                errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ? "" :
+                jQuery.parseJSON(jqXHR.responseText).message;
+            alert(errorString);
+        });
+    };
+</script>
 </body>
 </html>
